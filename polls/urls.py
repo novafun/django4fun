aaed061c616002ad579ddd70a236bf9598daeb5b
Hayden <haijing.ng@gmail.com>
2021-05-12 22:39:45 -0300

    update polls urls and index method of polls views

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 urlpatterns = [
     # The path() function is passed four arguments, two required: route and view, and two optional: kwargs, and name.
+    # ex: /polls/
     path('', views.index, name='index'),  # the index matches the function name in views.py
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote')
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from polls.models import Question
 
 
 # Create your views here.
 # To call the view, we need to map it to a URL - and for this we need a URLconf.
 # To create a URLconf in the polls directory, create a file called urls.py.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+
+def results(request, question_id):
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question  {question_id}.")
