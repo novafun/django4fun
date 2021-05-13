93523891ebdc2d2247c06a747959416c80f5f408
Hayden <haijing.ng@gmail.com>
2021-05-13 01:03:15 -0300

    refactor to Django generic views

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,11 @@ app_name = 'polls'
 urlpatterns = [
     # The path() function is passed four arguments, two required: route and view, and two optional: kwargs, and name.
     # ex: /polls/
-    path('', views.index, name='index'),  # the index matches the function name in views.py
+    path('', views.IndexView.as_view(), name='index'),  # the index matches the function name in views.py
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote')
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
-from django.template import loader
+from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
-from polls.models import Question, Choice
+from .models import Choice, Question
 
 
 # Create your views here.
@@ -11,31 +11,27 @@ from polls.models import Question, Choice
 # To create a URLconf in the polls directory, create a file called urls.py.
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
-    # return HttpResponse(output)
-    template = loader.get_template('polls/index.html')  # update our index view in polls/views.py to use the template
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    # for ListView, the automatically generated context variable is question_list.
+    # To override this we provide the context_object_name attribute,
+    # specifying that we want to use latest_question_list instead
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    # return HttpResponse(f"You're looking at question {question_id}.")
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
