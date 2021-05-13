95e845d2e521f286be4dc5a76b54a47053a9f4ce
Hayden <haijing.ng@gmail.com>
2021-05-13 00:46:39 -0300

    update results and vote

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
-from polls.models import Question
 from django.template import loader
+from django.urls import reverse
+
+from polls.models import Question, Choice
 
 
 # Create your views here.
@@ -32,9 +34,33 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = f"You're looking at the results of question {question_id}."
-    return HttpResponse(response)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse(f"You're voting on question  {question_id}.")
+    # return HttpResponse(f"You're voting on question  {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        # request.POST is a dictionary-like object that lets you access submitted data by key name.
+        # In this case, request.POST['choice'] returns the ID of the selected choice, as a string.
+        # request.POST values are always strings.
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # checks for KeyError and redisplays the question form with an error message if choice isn’t given.
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1  # a race condition might occur
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+
+        # The reverse function helps avoid having to hardcode a URL in the view function.
+        # It is given the name of the view that we want to pass control to ('polls:results')
+        # ,and the variable portion of the URL pattern that points to that view. [args=(question.id,)]
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
