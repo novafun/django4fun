a0f7a81ce5062f37071100f3b35dc258babbe63f
Hayden <haijing.ng@gmail.com>
2021-05-14 14:19:03 -0300

    Register the Choice class using stacked inline

--- polls/admin.py
+++ polls/admin.py
@@ -3,14 +3,20 @@ from .models import Choice, Question
 
 
 # Register your models here.
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+#  “Choice objects are edited on the Question admin page. By default, provide enough fields for 3 choices.”
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),  # Date information title
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),  # Date information title
     ]
+    inlines = [ChoiceInline]
 
 
 # create a model admin class, then pass it as the second argument to admin.site.register()
 admin.site.register(Question, QuestionAdmin)  # Make the poll app modifiable in the admin¶
-
-admin.site.register(Choice)
